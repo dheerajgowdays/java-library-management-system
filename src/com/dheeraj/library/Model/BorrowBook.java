1b115d3fd2fba98a8c5b3ec7083bff4d43f2a08d
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-07-17 23:38:56 +0530

    feat: add borrow book model

--- src/com/dheeraj/library/Model/BorrowBook.java
+++ src/com/dheeraj/library/Model/BorrowBook.java
@@ -1,22 +1,9 @@
 package com.dheeraj.library.Model;
 
 import java.time.LocalDate;
-public class BorrowBook extends Book {
-    private long memberId;
-    private LocalDate borrowDate;
 
-    public BorrowBook(long bookId, String bookName, String authorName, LocalDate publishedDate, String bookType, boolean isAvailable, long memberId, LocalDate borrowDate) {
-        super(bookId, bookName, authorName, publishedDate, bookType, isAvailable);
-        this.memberId = memberId;
-        this.borrowDate = borrowDate;
-    }
+public record BorrowBook(long recordId, long memberId, Book book, LocalDate borrowDate, LocalDate dueDate,
+                         LocalDate returnedDate) {
 
-    public void setborrowDate(LocalDate borrowDate){
-        this.borrowDate = borrowDate;
-    }
-    public LocalDate getborrowDate(){
-        return borrowDate;  
-    }
-    
-    
+
 }
